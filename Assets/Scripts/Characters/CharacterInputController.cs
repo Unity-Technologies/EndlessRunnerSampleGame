f9e273cec1bfca9845b3d8d92be8cdfad18c3f86
guanghui-zhang <guanghui.zhang@unity3d.com>
2020-09-02 11:10:52 +0800

    Update CharacterInputController.cs, we can oprate on webgl now

--- Assets/Scripts/Characters/CharacterInputController.cs
+++ Assets/Scripts/Characters/CharacterInputController.cs
@@ -177,7 +177,7 @@ public class CharacterInputController : MonoBehaviour
 
 	protected void Update ()
     {
-#if UNITY_EDITOR || UNITY_STANDALONE
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
         // Use key input in editor or standalone
         // disabled if it's tutorial and not thecurrent right tutorial level (see func TutorialMoveCheck)
 
