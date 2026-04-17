855cf63b6db5ecdfa6d69d93fa3ccfc31dba6f8b
Santiago Tangarife Rincón <santiago.tangarife@unity3d.com>
2026-04-17 13:34:28 -0500

    updated to 2022.3

--- Assets/Scripts/Consumable/Consumable.cs
+++ Assets/Scripts/Consumable/Consumable.cs
@@ -68,7 +68,7 @@ public abstract class Consumable : MonoBehaviour
             yield return op;
             m_ParticleSpawned = op.Result.GetComponent<ParticleSystem>();
             if (!m_ParticleSpawned.main.loop)
-                StartCoroutine(TimedRelease(m_ParticleSpawned.gameObject, m_ParticleSpawned.main.duration));
+                c.StartCoroutine(TimedRelease(m_ParticleSpawned.gameObject, m_ParticleSpawned.main.duration));
 
             m_ParticleSpawned.transform.SetParent(c.characterCollider.transform);
             m_ParticleSpawned.transform.localPosition = op.Result.transform.position;
